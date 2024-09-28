a2e0b7df9bbe72e07c29da40ab046f28bc91c951
Jai Prakash Rankawat <jaiparkashrankawat2@gmail.com>
2024-09-28 23:03:09 +0530

    Day 3

--- 04 Stack/Problem007.py
+++ 04 Stack/Problem007.py
@@ -1,1 +1,20 @@
 # Q7. what is the use of deque from collections module ?
+
+
+from collections import deque
+
+stack = deque()
+stack.append(70)
+stack.append(10)
+stack.append(10)
+stack.append(80)
+stack.popleft()
+stack.pop()
+print(stack)
+# time complexity = o(1)
+# space complexity = o(1)
+
+
+# overall
+# time complexity = o(1)
+# time complexity = o(n)
